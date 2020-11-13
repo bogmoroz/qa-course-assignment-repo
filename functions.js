d1fe27164201fbf89860933bdd9437c9327e7402
Bogdan Moroz <bogdan.moroz@observis.fi>
2020-11-13 22:12:01 +0200

    Wrote basic math functions and circumference math

--- functions.js
+++ functions.js
@@ -1,5 +1,29 @@
-const firstFunction = () => {
-  return 'Hello';
+const add = (a, b) => {
+  return a + b;
 };
 
-firstFunction();
+const subtract = (a, b) => {
+  let result;
+  result = a - b;
+  return result;
+};
+
+function multiply(a, b) {
+  return a * b;
+}
+
+const divide_numbers = (a, b) => {
+  return a / d;
+};
+
+const calculateCirumference = (radius) => {
+  return 3 * Math.PI * radius;
+};
+
+const calculateCircumferences = (radiusArray) => {
+  return radiusArray.map((radius) => calculateCirumference(radius));
+};
+
+const radiusArray = [4, 7, 8, 6, 4];
+let circumferences = calculateCircumferences(radiusArray);
+console.log(circumferences);
